343b166c4b9037da3cf233fde0fb89e2f82f544e
cisty <vue_0625399@my.scccd.edu>
2022-05-04 23:40:24 -0700

    Update vertex.h

--- PQAMatrix/vertex.h
+++ PQAMatrix/vertex.h
@@ -7,14 +7,13 @@ struct Vertex{
     int p;                                      // predecessor vertex
     bool c;                                     // vertex 'color' to indicate if visited
 
-    Vertex() : ID(-1), qID(-1), d(INT_MAX), p(0), c(0) {}
+    Vertex() : ID(-1), d(INT_MAX), p(0), c(0) {}
 
     bool operator<(const Vertex v) {return (this->d < v.d);}
     bool operator>(const Vertex v) {return (this->d > v.d);}
     bool operator==(const Vertex v) {return (this->ID==v.ID);}
     Vertex& operator=(const Vertex v) {
         this->ID = v.ID;
-        this->qID = v.qID;
         this->d = v.d;
         this->p = v.p;
         this->c = v.c;
